71b7128a6cb6c6165b3e2af461398ce67e08aefe
tianyuli <bzdlity@gmail.com>
2021-10-14 17:14:38 -0700

    upload selected data

--- utils/gen_human_demo.py
+++ utils/gen_human_demo.py
@@ -9,7 +9,7 @@ from fairmotion.ops import motion as motion_ops
 
 
 def gen_jump_1():
-    BVH_FILENAME = “PATH_TO_BVH_FILE”
+    BVH_FILENAME = './data/CMU/01/01_01_pose.bvh'
     motion = bvh.load(BVH_FILENAME)
     translated_motion = motion_ops.translate(motion, np.array([1, 1, 1]))
     sliced_motion = motion_ops.cut(translated_motion, 10, 20)
